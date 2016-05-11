6b7a3ae969cf1a449343c14780661fc8e75cf678
RKing <rosalyn.kingsmill@gmail.com>
2016-05-11 19:13:08 -0400

    starting screen with sheriff and mafia buttons

--- Mafia/ViewController.m
+++ Mafia/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +26,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
